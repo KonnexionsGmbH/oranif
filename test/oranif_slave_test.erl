5ceca538a4b48ace6d4ed5a422e9693f18d6986b
Karl Keiser <karl.keiser@k2informatics.ch>
2019-06-17 10:19:03 +0200

    fix tuples in dpi:dafe call, add error handling to call macro, create an own connection for the ping test

--- test/oranif_slave_test.erl
+++ test/oranif_slave_test.erl
@@ -3,6 +3,27 @@
 
 -define(SLAVE_NAME, dpi_slave).
 
+-define(execStmt(__Conn, __Sql),
+    (fun() ->
+        _Stmt = dpi:conn_prepareStmt(__Conn, false, __Sql, <<"">>),
+        try
+            dpi:stmt_execute(_Stmt, []),
+            dpi:stmt_release(_Stmt)
+        catch Class:Error ->
+            dpi:stmt_release(_Stmt),
+            {error, Class, Error}
+        end
+        
+    end)()
+).
+
+%% gets a value out of a fetched set, compares it using an assertation,
+%% then cleans is up again
+assert_getQueryValue(Stmt, Index, Value) ->
+    QueryValueRef = maps:get(data, (dpi:stmt_getQueryValue(Stmt, Index))),
+    ?assertEqual(Value, dpi:data_get(QueryValueRef)),
+	dpi:data_release(QueryValueRef),
+    ok.
 
 
 assert_getQueryInfo(Stmt, Index, Value, Atom) ->
@@ -523,10 +544,15 @@ commit_rollback({_Context, Conn}) ->
     assert_getQueryValue(Stmt3, 1, 1.0),
     dpi:stmt_release(Stmt3).
 
-ping_close({_Context, Conn}) -> 
-    ok = dpi:conn_ping(Conn),               %% valid connection: ping succeeds
-    ok = dpi:conn_close(Conn, [], <<"">>),  %% invalidate connection
-    ?assertException(error, {error, _}, dpi:conn_ping(Conn)), %% now the ping fails
+ping_close({Context, _Conn}) -> 
+    #{tns := Tns, user := User, password := Password} = getConfig(),
+    NewConn = dpi:conn_create(
+            Context, User, Password, Tns,
+            #{encoding => "AL32UTF8", nencoding => "AL32UTF8"}, #{}
+    ),
+    ok = dpi:conn_ping(NewConn),               %% valid connection: ping succeeds
+    ok = dpi:conn_close(NewConn, [], <<"">>),  %% invalidate connection
+    ?assertException(error, {error, _File, _Line, _Map}, dpi:conn_ping(NewConn)), %% now the ping fails
     ?assert(true).
 
 var_define({_Context, Conn}) -> 
@@ -828,8 +854,8 @@ distributed(_) ->
     Context = dpi:safe(dpi, context_create, [3, 0]),
     Conn = dpi:safe(dpi, conn_create, [Context, User, Password, Tns, #{}, #{}]),
 
-    ok = dpi:safe(fun simple_fetch_no_assert/1,[[Context, Conn]]),
-    ok = dpi:safe(fun var_bind_no_assert/1,[[Context, Conn]]).
+    ok = dpi:safe(fun simple_fetch_no_assert/1,[{Context, Conn}]),
+    ok = dpi:safe(fun var_bind_no_assert/1,[{Context, Conn}]).
 
 catch_error_message({_Context, Conn}) -> 
     Stmt = dpi:conn_prepareStmt(

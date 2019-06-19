4ff857ca7406f1090bd25b9d1ecc21e986cfa8a9
Karl Keiser <karl.keiser@k2informatics.ch>
2019-06-19 13:48:20 +0200

    add bindData job, add setup for running tests on one persistent dpi slave

--- test/oranif_SUITE.erl
+++ test/oranif_SUITE.erl
@@ -3,7 +3,9 @@
 -module(oranif_SUITE).
 
 -export([all/0, suite/0, init_per_suite/1, end_per_suite/1]).
--export([shortInserts/1, balancedInserts/1, heavyInserts/1, longInserts/1, longInserts/1]).
+-export([shortInserts/1, balancedInserts/1, heavyInserts/1, simultaneousInserts/1, longInserts/1]).
+-export([heavyData/1, longData/1]).
+-export([insertStress/1, dataMemStress/1]).
 
 -include_lib("common_test/include/ct.hrl").
 -include("test_common.hrl").
@@ -52,9 +54,10 @@ getConfig() ->
             error(Reason)
     end.
 
-all() -> [shortInserts, balancedInserts, heavyInserts, longInserts, longInserts].
-%all() -> [shortInserts].
--define(F(Fn, Runs, Conn, Stmt, Job), Fn(ConfigData) -> runFrame(ConfigData, Runs, Conn, Stmt, fun Job/5)).
+%all() -> [shortInserts, balancedInserts, heavyInserts, simultaneousInserts, longInserts].
+all() -> [insertStress, dataMemStress].
+-define(F(Fn, Runs, Conn, Stmt, Job), Fn(ConfigData) -> runFrame(ConfigData, Runs, Conn, Stmt, fun Job/5, false)).
+-define(N(Fn, Runs, Stmt, Job), Fn(ConfigData) -> runFrame(ConfigData, Runs, undefined, Stmt, fun Job/5, true)).
 
 %% defines the test cases. For each one, the amount of runs and the amount of
 %% connections per run as well as the job that each connection runs can be
@@ -68,6 +71,15 @@ all() -> [shortInserts, balancedInserts, heavyInserts, longInserts, longInserts]
 ?F(heavyInserts,           20,       3,        5000,      insertIntoTable).
 ?F(simultaneousInserts,    20,       100,      500,       insertIntoTable).
 ?F(longInserts,            200,      3,        500,       insertIntoTable).
+
+?F(heavyData,              20,       5,        2000,      bindData).
+?F(longData,               200,      5,        500,       bindData).
+
+%% "Native" test cases that only run on one slave node that is persistent
+%% as such, there's no "Conn" parameter because there's only one slave
+
+?N(insertStress,           200,               5000,      insertIntoTable).
+?N(dataMemStress,          500,               2000,      bindData).
 
 suite() ->
      [
@@ -94,12 +106,13 @@ end_per_suite(ConfigData) ->
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% takes all the parameters, then runs executes the right amount of runs
-runFrame(ConfigData, Runs, Connections, Statements, SlaveJob ) ->
+runFrame(ConfigData, Runs, Connections, Statements, SlaveJob, Native ) ->
     io:format(user, "AYYYYYYYYYYYYYYYYYYYYYYYYY ~p ~n", [?LINE]),
     [
     begin
         io:format(user, "Start Run ~p/~p ~n", [X, Runs]),
-        run(ConfigData, Connections, Statements, SlaveJob)
+        (case Native of false -> fun run/4; true -> fun native/4 end)
+            (ConfigData, Connections, Statements, SlaveJob)
     end
     || X <- lists:seq(1, Runs)].
 
@@ -112,6 +125,15 @@ run(ConfigData, Connections, Statements, SlaveJob) ->
     [spawn_link(fun() -> spawnJob(list_to_atom(lists:flatten(io_lib:format("miau~p", [X]))),
         lists:flatten(io_lib:format("bobbytables~p", [X])), User, Pswd, Tns, Master, Connections, Statements, SlaveJob) end) || X <- lists:seq(1, Connections)],
     multiReceive(Connections, Connections).
+
+
+%% Performs a run by creating the right amount of nodes and passing to each of
+%% them them the data required to run the job, then waits for each node's
+%% message
+native(ConfigData, _, Statements, SlaveJob) ->
+    {Tns, User, Pswd, Lang, Logging} = ?value(config, ConfigData),
+    ok = dpi:load(nativeSlave),
+    ok = dpi:safe(SlaveJob, ["bobbytables", User, Pswd, Tns, Statements]).
 
 %% loads a slave node and tells it to run the job, then when it is done, sends
 %% its own master the message
@@ -166,4 +188,54 @@ insertIntoTable(TableName, User, Pswd, Tns, Statements) ->
     call(Conn, ?B(["drop table ", TableName])),
     dpi:conn_release(Conn),
     dpi:context_destroy(Context),
+    ok.
+
+
+%% this job makes a table, inserts [Statements] rows, then tries to find all of
+%% those rows again and finally drops the table.
+bindData(TableName, User, Pswd, Tns, Statements) ->
+    Context = dpi:context_create(3, 0),
+    Conn = dpi:conn_create(Context, User, Pswd, Tns, #{}, #{}),
+    call(Conn, ?B(["drop table ", TableName]), false),
+    call(Conn, ?B(["create table ", TableName, " (a int, b int, c int)"])),
+    [
+        begin
+        Stmt = dpi:conn_prepareStmt(Conn, false, ?B(["insert into ", TableName, " values (:A, :B, :C)"]), <<"">>),
+        BindData = dpi:data_ctor(),
+        dpi:data_setInt64(BindData, X*2),
+        dpi:stmt_bindValueByPos(Stmt, 1, 'DPI_NATIVE_TYPE_INT64', BindData),
+        dpi:data_setInt64(BindData, X*3),     % data can be recycled
+        dpi:stmt_bindValueByPos(Stmt, 2, 'DPI_NATIVE_TYPE_INT64', BindData),
+        dpi:data_setInt64(BindData, X*5),
+        dpi:stmt_bindValueByPos(Stmt, 3, 'DPI_NATIVE_TYPE_INT64', BindData),
+        dpi:data_release(BindData),
+        dpi:stmt_execute(Stmt, []),
+        dpi:stmt_release(Stmt)
+        end
+     || X <- lists:seq(1, Statements)],
+
+    [
+        begin
+        Stmt2 = dpi:conn_prepareStmt(Conn, false, ?B(["select a, b, c from ", TableName, " where a = ", integer_to_list(2*X)]) , <<"">>),
+        3 = dpi:stmt_execute(Stmt2, []),
+        dpi:stmt_fetch(Stmt2),
+        %ok = assert_getQueryValue(Stmt2, 1, 2.0 * X),
+        %ok = assert_getQueryValue(Stmt2, 2, 3.0 * X),
+        %ok = assert_getQueryValue(Stmt2, 3, 5.0 * X),
+        dpi:stmt_release(Stmt2)
+        end
+    
+    || X <- lists:seq(1, Statements)],
+
+    dpi:conn_commit(Conn),
+    call(Conn, ?B(["drop table ", TableName])),
+    dpi:conn_release(Conn),
+    dpi:context_destroy(Context),
+    ok.
+
+assert_getQueryValue(Stmt, Index, Value) ->
+    QueryValueRef = maps:get(data, (dpi:stmt_getQueryValue(Stmt, Index))),
+    Value = dpi:data_get(QueryValueRef),
+	dpi:data_release(QueryValueRef),
+    
     ok.

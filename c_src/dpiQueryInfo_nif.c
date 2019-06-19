b402b8065e09a56594c958ad7635c519fb87d132
Bikram Chatterjee <chatterjee@bluewin.ch>
2019-06-19 12:46:08 +0200

    data resource free fixes

--- c_src/dpiQueryInfo_nif.c
+++ c_src/dpiQueryInfo_nif.c
@@ -85,6 +85,7 @@ DPI_NIF_FUN(queryInfo_delete)
 
     if ((!enif_get_resource(env, argv[0], dpiQueryInfo_type, &qRes)))
         return BADARG_EXCEPTION(0, "resource queryInfo");
+
     enif_release_resource(qRes);
     return ATOM_OK;
 }

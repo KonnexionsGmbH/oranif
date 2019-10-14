1e52d9df6c6b37d7b301be220931bce37a7cf717
Bikram Chatterjee <chatterjee@bluewin.ch>
2019-10-14 16:11:53 +0200

    Update travis.sql

--- test/travis.sql
+++ test/travis.sql
@@ -16,13 +16,13 @@ SET VERIFY OFF
 DEFINE logfile_dir = &1
 
 host mkdir &&logfile_dir
-CONNECT system/oracle@0.0.0.0:1521/xe
+CONNECT system/travis@0.0.0.0:1521/xe
 SET SHOWMODE OFF
 
 DEFINE spool_file = &&logfile_dir.hr_main.log
 SPOOL &spool_file
 
-CREATE USER scott IDENTIFIED BY regit;
+CREATE USER scott IDENTIFIED BY tiger;
 
 GRANT ALTER SYSTEM TO scott;
 GRANT CREATE SESSION TO scott;
